dfd2edd452d51bac1874cf6a8f61b9df23ec9f77
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-17 22:40:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,10 +13,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    if BRANDS.include?(brand) == false
-      BRANDS << brand
+  def brand
+    @brand
+    if BRANDS.include?(@brand) == false
+      BRANDS << @brand
     end
   end
 
